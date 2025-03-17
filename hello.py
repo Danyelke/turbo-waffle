be7d427f65919769e8147454b418942cad318816
Tomlet <118920510+Danyelke@users.noreply.github.com>
2025-03-17 14:31:10 +0100

    first lines yay

--- hello.py
+++ hello.py
@@ -1,2 +1,19 @@
-msg = "Roll a dice!"
-print(msg)
+import random
+
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    return roll
+
+while True:
+    players = input("Enter the number of players (1-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 1 <= players <= 4:
+            break
+    else:
+        print("read that again")
+
+print(players)
